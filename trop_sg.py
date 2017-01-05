05b02434aeaf470d7a1fae20acc15b076b31bfa2
Mithun Cheriyath <mcheriyath@prokarma.com>
2017-01-04 19:45:59 -0700

    Updated Troposphere for creating Security Groups using cloudformation from a json config

--- trop_sg.py
+++ trop_sg.py
@@ -2,7 +2,6 @@ from troposphere import Base64, Join
 from troposphere import Parameter, Ref, Template
 from troposphere import cloudformation
 import troposphere.ec2 as ec2
-import numpy as np
 
 import json
 import boto3
@@ -25,7 +24,7 @@ Template)
 jsondata = ''
 vpcdata = ''
 sgs = ''
-
+CidrIp = ''
 
 # Fetching values from json given as argument while this python is executed
 parser = argparse.ArgumentParser()
@@ -39,7 +38,7 @@ t.add_version("2010-09-09")
 t.add_description("CF to create Security Groups for %s" % jsondata['EnvInfo']['Name'])
 
 #Get VPC ID based on the environment using boto3
-boto_ec2 = boto3.resource('ec2',region_name='us-east-2')
+boto_ec2 = boto3.resource('ec2',region_name='%s' % jsondata['Tags']['Region'])
 vpcfilters = [{'Name':'tag-value', 'Values':['%s' % jsondata['Tags']['Name']]}]
 try:
     vpcdata = list(boto_ec2.vpcs.filter(Filters=vpcfilters))[0]
@@ -47,24 +46,34 @@ except IndexError as e:
     print >> sys.stderr, ("Boto can't find the VPC. Is it there? [%s]" % e)
     sys.exit(0)
 
-
 sgs = jsondata['securitygroups']
 
-def mksg(IpProtocol, FromPort, ToPort, CidrIp):
-    return(ec2.SecurityGroupRule(
+# Function to create ingress and egress rules from json
+def make_securitygroup_rules(IpProtocol, FromPort, ToPort, CidrIp, ReferenceSecurityGroup):
+    if ReferenceSecurityGroup == "":
+        return(ec2.SecurityGroupRule(
+            CidrIp = CidrIp,
             IpProtocol = IpProtocol,
             FromPort = FromPort,
-            ToPort = ToPort,
-            CidrIp = CidrIp
+            ToPort = ToPort
+    ))
+    else:
+        return(ec2.SecurityGroupRule(
+            SourceSecurityGroupId = Ref(ReferenceSecurityGroup),
+            IpProtocol = IpProtocol,
+            FromPort = FromPort,
+            ToPort = ToPort
     ))
 
-
+# Security Group Creation
+# Ref: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ec2-security-group-rule.html
 for sg in sgs:
     sgname = sg['Name']
     t.add_resource(ec2.SecurityGroup(
         sgname,
         GroupDescription = sg['Description'],
-        SecurityGroupIngress = [mksg(**rules) for rules in sg['Rules']],
+        SecurityGroupIngress = [make_securitygroup_rules(**rules) for rules in sg['iRules']],
+        SecurityGroupEgress = [make_securitygroup_rules(**rules) for rules in sg['eRules']],
         VpcId = vpcdata.vpc_id,
         Tags = Tags(**{
         'Name': '%s' % sg['Name'],
@@ -73,3 +82,4 @@ for sg in sgs:
     ))
 
 print(t.to_json())
+
